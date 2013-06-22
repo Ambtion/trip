a925920edd8f87670f157718c879e885ef2c50d2
Qu Ke <kequ@sohu-inc.com>
2013-06-22 18:02:41 +0800

    add base file

---
+++ AhaTrip/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AhaTrip
+//
+//  Created by Qu on 13-6-22.
+//  Copyright (c) 2013年 ke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AhaTrip/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AhaTrip
+//
+//  Created by Qu on 13-6-22.
+//  Copyright (c) 2013年 ke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
