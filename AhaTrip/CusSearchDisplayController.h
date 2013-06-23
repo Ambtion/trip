fddc578b1c54a7b123b27275fb2dc7f3fed495de
Qu Ke <kequ@sohu-inc.com>
2013-06-23 22:09:11 +0800

    right mune search

---
+++ AhaTrip/CusSearchDisplayController.h
@@ -0,0 +1,13 @@
+//
+//  CusSearchDisplayController.h
+//  AhaTrip
+//
+//  Created by Qu on 13-6-23.
+//  Copyright (c) 2013年 ke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CusSearchDisplayController : UISearchDisplayController
+
+@end
